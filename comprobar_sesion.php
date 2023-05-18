f1e69840fc2641d24dda5b836b79fe7b29afda13
Inax4 <ainhoacorral95@gmail.com>
2023-05-18 17:24:36 +0200

    segundo commit

--- comprobar_sesion.php
+++ comprobar_sesion.php
@@ -1,15 +1,16 @@
-<!-- si el usuario es gerente podrá acceder a las opciones que puede hacer el admin como eliminar o consultar pedidos a proveedor, consultar stock, añadir eventos   -->
 <?php
+// si el usuario es gerente podrá acceder a las opciones que puede hacer el admin como eliminar o consultar pedidos a proveedor, consultar stock, añadir eventos 
 $email = $_GET['email'];
 $pass = $_GET['password'];
 
 $db = new PDO('mysql:host=localhost; dbname=am', "root");
-$consul = $db->prepare("SELECT * FROM employees WHERE email='$email' AND password='$pass' AND position='Gerente'");
+$consul = $db->prepare("SELECT * FROM employees WHERE email=:email AND password=:password AND position='Gerente'");
 $consul->bindParam(':email', $email);
 $consul->bindParam(':password', $pass);
 $consul->execute();
 $colum = $consul->fetchColumn();
-if ($colum > 0) {
+
+if ($colum > 0) {  
     echo "Admin";
 
 } else {
